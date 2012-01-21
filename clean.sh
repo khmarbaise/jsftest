8fcbf6b27d8be9bef180d2596abb2b5741d6ddf8
Karl Heinz Marbaise <info@soebes.de>
2012-01-21 22:27:05 +0100

    - Removed unused script.

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find -type f -name ".classpath" -print0 | xargs -0 rm
-find -type f -name ".project" -print0 | xargs -0 rm
-find -type d -name ".settings" -print0 | xargs -0 rm -fr
-find -type d -name ".myeclipse" -print0 | xargs -0 rm -fr
-find -type d -name ".metadata" -print0 | xargs -0 rm -fr
